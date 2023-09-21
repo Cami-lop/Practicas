cc0b3c5903636f511601ff0623050aebf7ca6f85
Cami <camilalipez04@gmail.com>
2023-09-21 17:45:03 -0300

    Parcial solo falta ver lo de la posicion

--- Parcialito_corregido.R
+++ Parcialito_corregido.R
@@ -2,10 +2,10 @@
 
 getwd()   #veo donde esta mi directorio 
 #setwd("/home/clinux01/Descargas/CamiLabo/")  #Lo seteo para que est donde quiero
-
+setwd("C:/Users/Fernando Cabrera/Desktop/Cami_Labo/Practicas/")
 dias<-readline("Ingrese un dia del mes de Agosto")
 agosto<-1:31  #dias del mes de agosto
-temp<-c()
+temp<-0
 
 
 repeat{
@@ -17,24 +17,24 @@ repeat{
   }
 }
 
-if((dias>=1) | (dias<=31)){
  for(dia in agosto){
     temperatura<-as.numeric(readline("Ingrese temperatura en °C"))
-    temp<-as.numeric(c(temp,temperatura))
- }    
-}
+    temp[dia]<-temperatura 
+}    
+
 
 print(temp)    
 promedio_temp<-(mean(temp)) 
-    
+
+
 if(temperatura<promedio_temp){
     print(paste("El dia",dias,"del mes de Agosto la temperatura fue de",temperatura,"°C es decir debajo de la media mensual"))
  }else if(temperatura>promedio_temp){
-    print(paste("El dia",dias,"del mes de Agosto la temperatura fue de",temperatura,"es decir por encima  de la  media mensual"))
+    print(paste("El dia",dias,"del mes de Agosto la temperatura fue de",temperatura,"ºC es decir por encima  de la  media mensual"))
   }else if(temperatura==promedio_temp){
     print(paste("El dia",dias, "del mes de Agosto la temperatura fue de",temperatura,"ºC es decir igual a la media mensual"))
 }    
-  
+
 
 
 
