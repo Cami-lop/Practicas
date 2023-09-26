abf899fcd3d504362d9d0b5f84fa831e3e0a66c8
Cami <camilalipez04@gmail.com>
2023-09-26 13:01:35 -0300

    Listo para entregar 26/09

--- Parcialito_corregido_Final.R
+++ Parcialito_corregido_Final.R
@@ -1,16 +1,15 @@
 #Parcialito Camila Lopez
 
 getwd()   #veo donde esta mi directorio 
-#setwd("/home/clinux01/Descargas/CamiLabo/")  #Lo seteo 
+rm(list=ls())
+#setwd("/home/clinux01/Escritorio/CamiLabo/")  #Lo seteo 
 #setwd("C:/Users/Fernando Cabrera/Desktop/Cami_Labo/Practicas/") #compu casa
-
-#setwd("C:/Users/usuario/Downloads/")  #compu casa 2
 
 dias<-readline("Ingrese un dia del mes de Agosto")
 agosto<-1:31  #dias del mes de agosto
 temp<-0
 
-
+#Verifico si el dia ingresado es correcto
 repeat{
   if(dias<1 | dias>31 ){
     print("ERROR, EL NUMERO INGRESADO NO ES VALIDO")
@@ -22,23 +21,24 @@ repeat{
 
  for(dia in agosto){
     temperatura<-as.numeric(readline("Ingrese temperatura en °C"))
-    temp[dia]<-temperatura 
-    if(dias==dia){
+  while(is.na(temperatura)){   #puse el while para que se repita hasta que deje de cumplir la conicion de is.na
+    temperatura<-as.numeric(readline("Ingrese la temperatura nuevamente en °C"))    
+  }
+    temp<-temp+temperatura
+    if (dias==dia){
       temp_analisis<-temperatura 
   }
 }    
 
-
-print(temp)    
-
-promedio_temp<-round(mean(temp)) 
+#Promedio de la temperatura
+promedio_temp<-temp/31
 
 
 if(temp_analisis<promedio_temp){
     cat("El dia",dias,"del mes de Agosto la temperatura fue de",temp_analisis,"°C es decir debajo de la media mensual","\n")
- }else if(temp_analisis>promedio_temp){
+ } else if(temp_analisis>promedio_temp) {
     cat("El dia",dias,"del mes de Agosto la temperatura fue de",temp_analisis,"ºC es decir por encima  de la  media mensual","\n")
-  }else if(temp_analisis==promedio_temp){
+  } else {
    cat("El dia",dias,"del mes de Agosto la temperatura fue de",temp_analisis,"ºC es decir igual a la media mensual","\n")
  }    
 
