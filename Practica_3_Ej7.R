e96af206a73432117ab71ff2d8c45d22357a12f4
Camila <camilalipez04@gmail.com>
2023-10-12 13:03:47 -0300

    Hasta el ej 7e

--- Practica_3_Ej7.R
+++ Practica_3_Ej7.R
@@ -1,11 +1,14 @@
 getwd()
-setwd("C:/Users/Fernando Cabrera/Desktop/Cami_Labo/Practicas/")
+rm(list=ls())
+#setwd("C:/Users/Fernando Cabrera/Desktop/Cami_Labo/Practicas/")
+setwd("/home/clinux01/Escritorio/CamiLabo/Practicas/Datos Practica 3-20231011/")
 ############Ejercicio 7###########
 
-#temp <-scan("datos_tmedia_SABE_2010.txt",sep="") no me corre con esta funcion 
-archivo<-"C:/Users/Fernando Cabrera/Desktop/Cami_Labo/Practicas/Datos Practica 3-20231011/datos_tmedia_SABE_2010.txt"
+Temp_media <-scan("datos_tmedia_SABE_2010.txt",sep="") #en casa no me corre esta pero en la facu si 
+datos_temp<-data.frame(Temp_media)
+#archivo<-"C:/Users/Fernando Cabrera/Desktop/Cami_Labo/Practicas/Datos Practica 3-20231011/datos_tmedia_SABE_2010.txt"
+#datos_temp<- read.table(archivo,header = TRUE, col.name="Temp_media")
 
-datos_temp<- read.table(archivo,header = TRUE, col.name="Temp_media")
 #a)Dado que se trata de temperaturas medias mensuales para la Ciudad de Buenos Aires, valores
 #superiores a 40◦C son, muy probablemente, erroneos de acuerdo con el comportamiento climatologico de esta variable. Dise˜nar y programar un algoritmo que identifique la posici´on dentro
 #de la serie de los dıas donde el valor de la temperatura media supera los 40◦C, pero que ademas
@@ -25,7 +28,7 @@ valores_antes<-c()
 for(valor in posiciones_erroneas){
  valores_antes<-datos_temp[valor-1,]
  valores_desp<-datos_temp[valor+1,]
-  valores_erroneos<-c(valores_antes,valores_desp,valores_erroneos)
+  valores_erroneos<-c(valores_erroneos,valores_antes,valores_desp)
 }  
 
 print(valores_erroneos)# preguntar porque se guarda de atras para adelante 
@@ -39,8 +42,9 @@ extremos<-c()
 dato_faltante<- -999.0 
 datos_temp[,1][datos_temp[,1]==dato_faltante]<-NA
 datos_temp[,1][datos_temp[,1]>40]<-NA
-for(valores in datos_temp[,1]){
-  if(as.numeric(valores) >40){
+
+for(valores in Temp_media){
+  if(!is.na(valores)){
     valor_max<-max(datos_temp[,1], na.rm = T)
     valor_min<-min(datos_temp[,1], na.rm= T)
     extremos<-c(valor_min,valor_max)
@@ -49,13 +53,42 @@ for(valores in datos_temp[,1]){
   }
 }  
 
-#no entiendo porque se traba
-
 #c)Ordenar la serie de menor a mayor y calcular su mediana.
 rm(list=ls())
 datos_temp[,1]
-datos_temp_ordenado<-datos_temp[order(datos_temp$Temp_media,)]
-print(datos_temp_ordenado)
+datos_temp_ordenado<-sort(Temp_media)
+print(datos_temp_ordenado) #no da y deberia dar
+#mediana 
+datos_temp_ordenado_df<-data.frame(datos_temp_ordenado)
+mediana_temp<-apply(datos_temp_ordenado_df,2,median) 
+#aclaracion<- no se porque no saca los datos NA
 
-#no da y deberia dar
-dim(datos_temp)
+#d)Generar una serie de medias semanales a partir de los datos medios diarios de un data frame de dimensiones [1,1]
+x<-seq(1,365, by=7)
+media_semanal<-c()
+for (valor in x) {
+  semana <- datos_temp[valor:(valor + 6), 1]
+  media <- mean(semana, na.rm = TRUE)
+  media_semanal <- c(media_semanal,media)
+}
+
+#e)Dividir el rango de la variable en N intervalos de igual longitud y calcular el numero de elementos
+#de la serie que cae dentro de cada intervalo. Repetir el ejercicio utilizando la funcion intrınseca
+#“hist”
+
+#preguntar 
+
+y<-seq(1,365,by=5)  #son 73 datos
+anio<-c()
+for (valor in y) {
+  intervalo<- datos_temp[valor:(valor + 4), 1]
+  
+}
+
+#tengo que contar el largo de cada intervalo y ver los valores de temp que caen en cada intervalo
+
+
+
+
+
+
